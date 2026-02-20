728848cab2c839dc1bf0027c115fba5835ebba18
Rayan Brenno <rayanbrennodev@gmail.com>
2026-02-20 09:34:38 -0300

    done

--- src/main.py
+++ src/main.py
@@ -3,16 +3,16 @@ from metrics import compute_series_from_landmarks, compute_global_metrics
 from scoring import score_row_two_notes
 
 
-video_path = "data/raw/erradoE.mp4"
+video_path = "data/raw/errado5.mp4"
 
 frames = extract_landmarks_from_video(
     video_path=video_path,
-    side="left",
+    side="right",
     visibility_threshold=0.5,
     save_csv=True,
-    csv_path="outputs/landmarks_erradoE.csv",
+    csv_path="outputs/landmarks_errado5.csv",
     save_annotated_video=True,
-    annotated_video_path="outputs/erradoE.mp4"
+    annotated_video_path="outputs/errado5.mp4"
 )
 
 series = compute_series_from_landmarks(frames, smooth_window=5)
